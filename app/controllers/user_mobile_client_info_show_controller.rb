b38733403fd91b9b8a56fbd463d1157cbf980729
LaiJin <LuaJin312@gmail.com>
2014-01-17 17:44:04 +0800

    报名页面

--- app/controllers/user_mobile_client_info_show_controller.rb
+++ app/controllers/user_mobile_client_info_show_controller.rb
@@ -12,6 +12,11 @@ class UserMobileClientInfoShowController < ApplicationController
   end
 
   def activity_sign_up_list_view
+    @activity_sign_ups = ActivitySignUp.where(:user_name => current_user.name, :activity_name => params[:activity_name]).paginate(page: params[:page], :per_page => PER_PAGE_COUNT)
+    @count = USER_NUMBER_INIT
+    if params[:page]
+      @count = Integer(((Integer(params[:page]) - 1) * PER_PAGE_COUNT))
+    end
   end
 
   def detailed_once_bid_view
